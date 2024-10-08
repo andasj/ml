15a1ef82b627e69fb6afdf2f4345b018378b4e01
andasj <andersonsj@outlook.com>
2024-10-07 22:07:33 -0400

    Rafactor: Cleaning function decision trees.

--- example_decision_trees.py
+++ example_decision_trees.py
@@ -1,71 +1,33 @@
-# Manipulação de dados
 import numpy as np
 import pandas as pd
-
-# Visualização de dados
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-# Divisão dos dados
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+import warnings
+warnings.filterwarnings('ignore')
 
 # Algoritmos de Machine Learning
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
-# Métricas de performance
-from sklearn import metrics
-from sklearn.metrics import (f1_score,
-                             accuracy_score,
-                             recall_score,
-                             precision_score,
-                             confusion_matrix,
-                             roc_auc_score)
+def prepare_dataset_credito(data):
 
-# Ajustes de Hiperparametros
-from sklearn.model_selection import GridSearchCV
+    # print(f'Esta base de dados tem {data.shape[0]} linhas e {data.shape[1]} colunas.')
+    # print(data.describe())
+    # print(data.info())
 
-# Optional para Annotations das funções
-from typing import Optional
-
-# Ignorar alertas
-import warnings
-
-warnings.filterwarnings('ignore')
-
-def prepare_dataset(data):
-
-    print(data.head())
-
-    # Verifica o shape dos dados
-    print(f'Shape dos dados: {data.shape}\n')
-
-    print(f'Esta base de dados tem {data.shape[0]} linhas e {data.shape[1]} colunas.')
-
-    print(data.describe())
-
-    # Lista de variáveis categóricas
-    colunas_cat = data.select_dtypes(include=['object']).columns.tolist()
-
-    # Loop para imprimir a contagem de valores únicos em cada coluna categórica
-    for coluna in colunas_cat:
-        print(f'### Coluna <{coluna}> ###')
-        print(data[coluna].value_counts())
-        print('-' * 40)
-
-    print(data.info())
-
-    # Verificando dados nulos
-    print('Colunas com dados nulos:')
-    print(data.isnull().sum()[data.isnull().sum() > 0])
+    # # Loop para imprimir a contagem de valores únicos em cada coluna categórica
+    # for coluna in data.select_dtypes(include=['object']).columns.tolist():
+    #     print(f'\n### Coluna <{coluna}> ###')
+    #     print(data[coluna].value_counts())
+    #     print('-' * 40)
 
     # Corrige o erro de digitação
     corrige_carro = {'carr0': 'carro'}
     data.replace(corrige_carro, inplace=True)
-
-    # Verifica as categorias novamente
-    print(data['motivo'].value_counts())
 
     # Convertendo variáveis Categóricas Ordinais
     conversao_variaveis = {
@@ -106,38 +68,22 @@ def prepare_dataset(data):
 
     # Gera a lista de variáveis categóricas
     cols_cat = data.select_dtypes(include='object').columns.tolist()
-
-    # Removendo 'inadimplente' pois é nossa variável Alvo
     cols_cat.remove('inadimplente')
-
-    print(cols_cat)
-
-    # Implementa o OneHotEncoding
     data = pd.get_dummies(data, columns=cols_cat, drop_first=True)
 
-    print(data.head())
-
     # Convertendo a variável alvo
-    conversao_alvo = {
-        'inadimplente': {'nao': 0, 'sim': 1}
-    }
-
+    conversao_alvo = {'inadimplente': {'nao': 0, 'sim': 1}}
     data.replace(conversao_alvo, inplace=True)
-    print(data['inadimplente'])
 
     # Imputando os valores nulos com a média
     data = data.fillna(data.mean())
 
-    # Verifica valores nulos novamente
-    print(data.isnull().sum())
-
-    print(data.head())
-
-    # Variáveis independentes (características)
     data_x = data.drop(['inadimplente'], axis=1)
-
-    # Variável dependente (alvo)
     data_y = data['inadimplente']
+
+    # print('\nDescribe dataset x:\n',data_x.describe())
+    # print(data_x.info())
+    # print('\nDescribe dataset y:\n',data_y.describe())
 
     return data_x, data_y
 
@@ -151,9 +97,10 @@ def train_trees(dataset_input_x, dataset_input_y):
                                                         stratify=dataset_input_y)  # mantém as proporções das classes
 
     # Instanciando o Modelo
-    # arvore_d = DecisionTreeClassifier(random_state=1, max_depth=7, max_leaf_nodes=10, min_impurity_decrease=0.001)
+    arvore_d = DecisionTreeClassifier(random_state=1, max_depth=7, max_leaf_nodes=10,
+                                      min_impurity_decrease=0.001, criterion="log_loss", splitter="best")
     # arvore_d = RandomForestClassifier(n_estimators=100, max_depth=9)
-    arvore_d = ExtraTreesClassifier(n_estimators=200, max_depth=11)
+    # arvore_d = ExtraTreesClassifier(n_estimators=200, max_depth=11)
 
     # Resultados dataset teste DecisionTreeClassifier:
     # Acurácia na base de teste: 72.0%
@@ -192,33 +139,36 @@ def train_trees(dataset_input_x, dataset_input_y):
     f1_score_test = metrics.f1_score(dataset_y_test, dataset_y_test_pred)
 
     print('Resultados dataset treino:')
+    print(f'Matriz de confusão de treino:\n {cm_train}')
     print(f'Acurácia na base de treino: {round(acc_train, 2) * 100}%')
     print(f'Precisão na base de treino: {round(prec_train, 2) * 100}%')
     print(f'Recall na base de treino: {round(rec_train, 2) * 100}%')
     print(f'F1 score na base de treino: {round(f1_score_train, 2) * 100}%')
 
-    # Plot da Matriz de Confusão
-    plt.figure(figsize=(7, 5))
-    sns.heatmap(cm_train, annot=True, fmt='g')
-    plt.title('Matriz de Confusão: Base de Treino', weight='bold')
-    plt.xlabel('Valores Previstos')
-    plt.ylabel('Valores Reais')
-    plt.show()
+    # # Plot da Matriz de Confusão
+    # plt.figure(figsize=(7, 5))
+    # sns.heatmap(cm_train, annot=True, fmt='g')
+    # plt.title('Matriz de Confusão: Base de Treino', weight='bold')
+    # plt.xlabel('Valores Previstos')
+    # plt.ylabel('Valores Reais')
+    # plt.show()
 
     print('\nResultados dataset teste:')
+    print(f'Matriz de confusão de teste:\n {cm_test}')
     print(f'Acurácia na base de teste: {round(acc_test, 2) * 100}%')
     print(f'Precisão na base de teste: {round(prec_test, 2) * 100}%')
     print(f'Recall na base de teste: {round(rec_test, 2) * 100}%')
     print(f'F1 score na base de teste: {round(f1_score_test, 2) * 100}%')
-    print(metrics.classification_report(dataset_y_test, dataset_y_test_pred))
 
-    # Plot da Matriz de Confusão
-    plt.figure(figsize=(7, 5))
-    sns.heatmap(cm_test, annot=True, fmt='g')
-    plt.title('Matriz de Confusão: Base de Teste', weight='bold')
-    plt.xlabel('Valores Previstos')
-    plt.ylabel('Valores Reais')
-    plt.show()
+    # print(metrics.classification_report(dataset_y_test, dataset_y_test_pred))
+
+    # # Plot da Matriz de Confusão
+    # plt.figure(figsize=(7, 5))
+    # sns.heatmap(cm_test, annot=True, fmt='g')
+    # plt.title('Matriz de Confusão: Base de Teste', weight='bold')
+    # plt.xlabel('Valores Previstos')
+    # plt.ylabel('Valores Reais')
+    # plt.show()
 
     # feature_names = list(dataset_x_train.columns)
     # plt.figure(figsize=(20, 30))
@@ -230,8 +180,6 @@ def train_trees(dataset_input_x, dataset_input_y):
 if __name__ == '__main__':
 
     dataset_complete = pd.read_csv('credito.csv')
-    dataset_x, dataset_y = prepare_dataset(dataset_complete)
+    dataset_x, dataset_y = prepare_dataset_credito(dataset_complete)
 
     train_trees(dataset_x, dataset_y)
-
-
